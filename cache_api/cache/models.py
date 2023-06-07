215d2dbe1584e6925bd46acd40d89a65e65ad036
NaghamMasalmeh <nagham.masalmah@exalt.ps>
2023-06-07 11:16:56 +0300

    Save cache objects to mongo db

--- cache_api/cache/models.py
+++ cache_api/cache/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class CacheEntry(models.Model):
+    key = models.PositiveIntegerField()
+    value = models.CharField(max_length=30)
+    access_time = models.TimeField()
--- cache_api/cache/views.py
+++ cache_api/cache/views.py
@@ -11,15 +11,28 @@
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .LRUcache import LRUCache
+from pymongo import MongoClient
+import time
 
-cache = LRUCache(3)
+mongo_client = MongoClient("mongodb://localhost:27017")
+db = mongo_client['cache_db']
+collection = db['cache']
+collection.delete_many({})
+
+cache_size = 3
+cache = LRUCache(cache_size)
 
 def index(request):
     return HttpResponse("Cache Homepage.")
 
+#Memory Cache APIs
 @csrf_exempt
 def put_api(request):
+    """
+    This API is used to insert items to the cache
+    """
     if request.method == "PUT":
+        #get key and value from request body and insert them to the cache
         data = request.body.decode('utf-8')
         data_dict = dict(item.split("=") for item in data.split("&"))
         if 'key' in data_dict and 'value' in data_dict:
@@ -28,6 +41,9 @@ def put_api(request):
 
 @csrf_exempt
 def get_api(request):
+    """"
+    This API is used to get the value of the provided key from the cache
+    """
     if request.method == "GET":
         key = request.GET.get('key', None)
         if key is not None:
@@ -38,3 +54,27 @@ def get_api(request):
                 return JsonResponse({'error': 'Key not found'}, status=404)
         else:
             return JsonResponse({'error': 'Key is not provided'}, status=400)
+
+#DB Cache APIs
+def put_in_db(key, value):
+    """
+    Insert key-value pair to the db, if the number of documents inside db is greater than cache size
+    it will remove the LRU by finding the document with the oldest access time.
+    """
+    documents_count = collection.count_documents({})
+    if documents_count == cache_size:
+        oldest_document = collection.find_one(sort=[('access_time', 1)], limit=1)
+        collection.delete_one({'_id': oldest_document['_id']})
+    collection.update_one({'key': key}, {'$set': {'value': value, 'access_time': time.time()}}, upsert=True)
+    return HttpResponse({'Document successfully inserted to the DB!'})
+
+def get_from_db(key):
+    """
+    Get the key for the required docuemnt, and search in the db
+    if the key exists it will return its value
+    """
+    cache_item = collection.find_one({'key': key})
+    if cache_item is None:
+        return JsonResponse({'error': 'Key not found'})
+    else:
+        return JsonResponse({'key': key, 'value': cache_item['value']})
